af3016dc026fabc5411a8b5422f28e75b3c765eb
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-01-06 18:47:17 +0200

    Created price section and starting next section

--- app/js/index.js
+++ app/js/index.js
@@ -4,7 +4,10 @@ $('.js-about-slider').slick({
     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
-
+$('.js-review-slider').slick({
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
 // $('.prev').click(function(){
 //     $('.slider').slick('slickPrev');
 // });
