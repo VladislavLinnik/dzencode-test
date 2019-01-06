d01ddab7f56b9e9f3ae460d14d0467280b306e66
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-01-06 13:05:42 +0200

    Created adbout section and slider

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,14 @@
 "use strict"
 
+$('.js-about-slider').slick({
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
+
+// $('.prev').click(function(){
+//     $('.slider').slick('slickPrev');
+// });
+  
+// $('.next').click(function(){
+//     $('.slider').slick('slickNext');
+// });
