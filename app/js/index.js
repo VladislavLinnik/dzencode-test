f4ea063983916d92266cfddf37cd7bacda5ef809
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-01-07 13:36:20 +0200

    Created 2 sections: prices and reviews. Slider does not work correctly

--- app/js/index.js
+++ app/js/index.js
@@ -5,13 +5,15 @@ $('.js-about-slider').slick({
     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
 $('.js-review-slider').slick({
-    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    dots: true,
+    appendArrows: '.slider-arrows',
+    prevArrow:"<button type='button' class='bottom-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='bottom-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
-// $('.prev').click(function(){
+// $('.bottom-prev').click(function(){
 //     $('.slider').slick('slickPrev');
 // });
   
-// $('.next').click(function(){
+// $('.bottom-next').click(function(){
 //     $('.slider').slick('slickNext');
 // });
