506caa81d9b0ece869d36451a27cf97eb5b56dc7
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-01-07 21:28:50 +0200

    Made highlighting cards in the price block

--- app/js/index.js
+++ app/js/index.js
@@ -10,6 +10,23 @@ $('.js-review-slider').slick({
     prevArrow:"<button type='button' class='bottom-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='bottom-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
+
+$('.js-btn-1').hover(function(){
+    $('.js-text-1').css('color', '#000');
+}, function() {
+    $('.js-text-1').css('color', '#fff');
+});
+$('.js-btn-2').hover(function(){
+    $('.js-text-2').css('color', '#000');
+}, function() {
+    $('.js-text-2').css('color', '#fff');
+});
+$('.js-btn-3').hover(function(){
+    $('.js-text-3').css('color', '#000');
+}, function() {
+    $('.js-text-3').css('color', '#fff');
+});
+
 // $('.bottom-prev').click(function(){
 //     $('.slider').slick('slickPrev');
 // });
