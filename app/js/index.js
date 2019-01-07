a6bf8fa64f7f29caa6b07ee94b013307a154d565
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-01-07 21:58:14 +0200

    Added smoothing scroll by these anchors

--- app/js/index.js
+++ app/js/index.js
@@ -39,3 +39,20 @@ $(window).scroll(function(){
         sticky.removeClass('fixed');
     }
 });
+
+// Smooth transition by anchors
+$("#smooth").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор блока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 500 мс
+    $('body,html').animate({scrollTop: top}, 500);
+});
+// P.S. До этого скрипта был баг: при переходе по якорю
+// на блок shop (prices) терялся верхний padding.
